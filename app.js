97357b78236643a11462f71ca528c16ed85ac57a
Sneha-Porwal <porwalsneha5@gmail.com>
2024-11-16 10:19:11 +0530

    changes done

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const { log } = require('console');
 const con = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'1234',
+    password:'Tiger',
     database:'onlinemenu'
 })
 
